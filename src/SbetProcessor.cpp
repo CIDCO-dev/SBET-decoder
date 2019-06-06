bdff62b3558cc7ae69ed67bb411c32165151460b
emile <emile@FUNDY.cidco.ca>
2019-06-06 14:28:48 -0400

    Documentation de SbetProcessor.hpp

--- src/SbetProcessor.cpp
+++ src/SbetProcessor.cpp
@@ -3,10 +3,16 @@
 
 #include "SbetProcessor.hpp"
 
+/**
+ * Create a SbetProcessor
+ */
 SbetProcessor::SbetProcessor(){
 
 }
 
+/**
+ * Destroy the SbetProcessor
+ */
 SbetProcessor::~SbetProcessor(){
 
 }
