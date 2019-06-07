7b0d4fd412df1a36daca4efe36d7a4c2d3cf49f8
emile <emile@FUNDY.cidco.ca>
2019-06-07 15:12:17 -0400

    Ajout de tests SbetProcessorTest.hpp

---
+++ test/SbetProcessorTest.hpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   SbetProcessorTest.hpp
+ * Author: emile
+ *
+ * Created on June 7, 2019, 9:06 AM
+ */
+#include "catch.hpp"
+#include "../src/SbetPrinter.hpp"
+#include "../src/AccuracyPrinter.hpp"
+#include "../src/AccuracyProcessor.hpp"
+#include "../src/SbetProcessor.hpp"
+
+TEST_CASE("test the readFile function of the SbetProcessor")
+{
+    SbetPrinter sp;
+    std::string name = "ejoiqjd";
+}
+
